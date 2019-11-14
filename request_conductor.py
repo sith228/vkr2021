d1cf124298c23719554eb8d599acb3765154a20c
MIkhail-Zoreev <mzoreev@gmail.com>
2019-11-14 21:09:59 +0300

    Testing using log

--- request_conductor.py
+++ request_conductor.py
@@ -1,5 +1,7 @@
 import os
 import time
+import logging
+import logging.config
 
 import cv2
 import time
@@ -8,45 +10,77 @@ from flask import Flask, request, Response
 from urllib.request import Request, urlopen
 
 
-# def test_request(img, url="http://127.0.0.1:5000/save_image"):
-#     '''
-#     Send jpg image to url in http post request
-#     :param img: numpy array of image
-#     :param url: url
-#     :return: nothing
-#     '''
-#     headers = {'content-type': 'image/jpeg'}
-#     req = request.Request(url, data=img, headers=headers)
-#     request.urlopen(req)
+def init_logger():
+    logger_config = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'default': {
+                'format': '%(message)s',
+                'datefmt': '%d.%m %H:%M:%S',
+            }, 'net': {
+                'format': '%(message)s',
+                'datefmt': '%m-%d %H:%M:%S',
+            }
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default'
+            },
+            'summary': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': "log/test_session_" + str(time.strftime("%Y-%m-%d %H-%M", time.gmtime())) + ".log",
+                'encoding': 'utf-8',
+                'mode': 'w',
+                'formatter': 'default'
+            }
+        },
+        'loggers': {
+            'main': {
+                'handlers': ['summary', 'console'],
+                'level': 'DEBUG',
+                'propagate': True
+            }
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'summary']
+        }
+    }
+
+    logging.config.dictConfig(logger_config)
+
 
 def generate_image_table(path, table):
     for i in os.listdir(path):
         if i.endswith(".jpg") or i.endswith(".png") or i.endswith(".jpeg"):
-            table.write("\"" + path + "/" + i + "\"" + "\n")
+            table.write(path + "|" + "\n")
         elif os.path.isdir(path + "/" + i):
             generate_image_table(path + "/" + i, table)
 
 
 def start_test(path):
     table = open(path, 'r')
-    log_file = open("log/test_session_" + str(time.strftime("%Y-%m-%d %H-%M", time.gmtime())) + ".log", 'w')
     right_answers = 0
     files_count = 0
     for line in table:
         files_count += 1
-        image_path = line[1: line.find("\"", 1)]
-        image_label = line[line.find("\" ") + 2 : len(line) - 1]
+        line = line.split('|')
+        image_path = line[0]
+        image_label = line[1].rstrip('\n')
         image_answer = test_request(cv2.imread(image_path), "http://127.0.0.1:5000/save_image")
         image_answer = image_answer.decode("utf-8")
         if image_answer == image_label:
-            log_file.write("[SUCCESS] ")
+            log.debug("[RIGHT] Expected: %3s Received: %4s %s" % (image_label, image_answer, image_path))
             right_answers += 1
         else:
-            log_file.write("[FAILED] ")
-        log_file.write(image_path + " Expected: " + image_label + " Recieved " + str(image_answer) + "\n")
-    log_file.write("Files: %d   Right: %d   Accuracy: %d" % (files_count, right_answers, right_answers / files_count))
-    log_file.close()
+            log.debug("[WRONG] Expected: %3s Received: %4s %s" % (image_label, image_answer, image_path))
+    log.debug("Right: %d, All: %d, Accuracy: %f" % (right_answers, files_count, right_answers / files_count))
     table.close()
+
 
 def test_request(img, url):
     """
@@ -88,15 +122,9 @@ def test_request(img, url):
     print("done")
 
 
-# Local Url
-url = "http://127.0.0.1:5000/save_image"
-# Photo location
-image_path = "test/IMG_20191024_142434.jpg"
-# "testImage.jpg"
-# Read photo
-photo = cv2.imread(image_path)
-# Test POST http request
-# test_request(photo, url=url)
+init_logger()
+log = logging.getLogger("root")
+
 
 # Generate table template
 # table = open("test/table.txt", 'w')
