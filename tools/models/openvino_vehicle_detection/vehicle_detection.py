031f5084d170c44535249a5583746ae35f20275c
Mikhail-Zoreev <mzoreev@gmail.com>
2020-04-14 00:47:56 +0300

    vehicle detection improve

--- tools/models/openvino_vehicle_detection/vehicle_detection.py
+++ tools/models/openvino_vehicle_detection/vehicle_detection.py
@@ -32,100 +32,65 @@ def build_argparser():
 
     return parser
 
+class OVVehicleDetector():
+    def __init__(self):
+        model_xml = os.path.join("tools", "models", "openvino_vehicle_detection", "vehicle-detection-adas-0002.xml")
+        model_bin = os.path.join("tools", "models", "openvino_vehicle_detection", "vehicle-detection-adas-0002.bin")
+        #Place for your CPU extensions
+        #ie.add_extension(args.cpu_extension, "CPU"
+        self.net = IENetwork(model=model_xml, weights=model_bin)
+        self.ie = IECore()
 
-def main():
-    log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
-    args = build_argparser().parse_args()
-    model_xml = args.model
-    model_bin = os.path.splitext(model_xml)[0] + ".bin"
+        img_info_input_blob = None
+        self.feed_dict = {}
+        for blob_name in self.net.inputs:
+            if len(self.net.inputs[blob_name].shape) == 4:
+                self.input_blob = blob_name
+            elif len(self.net.inputs[blob_name].shape) == 2:
+                img_info_input_blob = blob_name
+            else:
+                raise RuntimeError("Unsupported {}D input layer '{}'. Only 2D and 4D input layers are supported"
+                                   .format(len(self.net.inputs[blob_name].shape), blob_name))
 
-    log.info("Creating Inference Engine...")
-    ie = IECore()
-    if args.cpu_extension and 'CPU' in args.device:
-        ie.add_extension(args.cpu_extension, "CPU")
-    # Read IR
-    log.info("Loading network files:\n\t{}\n\t{}".format(model_xml, model_bin))
-    net = IENetwork(model=model_xml, weights=model_bin)
+        assert len(self.net.outputs) == 1, "Demo supports only single output topologies"
 
-    if "CPU" in args.device:
-        supported_layers = ie.query_network(net, "CPU")
-        not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
-        if len(not_supported_layers) != 0:
-            log.error("Following layers are not supported by the plugin for specified device {}:\n {}".
-                      format(args.device, ', '.join(not_supported_layers)))
-            log.error("Please try to specify cpu extensions library path in sample's command line parameters using -l "
-                      "or --cpu_extension command line argument")
-            sys.exit(1)
+        self.out_blob = next(iter(self.net.outputs))
+        log.info("Loading IR to the plugin...")
+        self.exec_net = self.ie.load_network(network=self.net, num_requests=2, device_name="CPU")
+        self.n, self.c, self.h, self.w = self.net.inputs[self.input_blob].shape
+        if img_info_input_blob:
+            self.feed_dict[img_info_input_blob] = [h, w, 1]
 
-    img_info_input_blob = None
-    feed_dict = {}
-    for blob_name in net.inputs:
-        if len(net.inputs[blob_name].shape) == 4:
-            input_blob = blob_name
-        elif len(net.inputs[blob_name].shape) == 2:
-            img_info_input_blob = blob_name
-        else:
-            raise RuntimeError("Unsupported {}D input layer '{}'. Only 2D and 4D input layers are supported"
-                               .format(len(net.inputs[blob_name].shape), blob_name))
+    def get_boxes(self, image):
 
-    assert len(net.outputs) == 1, "Demo supports only single output topologies"
-
-    out_blob = next(iter(net.outputs))
-    log.info("Loading IR to the plugin...")
-    exec_net = ie.load_network(network=net, num_requests=2, device_name=args.device)
-    # Read and pre-process input image
-    n, c, h, w = net.inputs[input_blob].shape
-    if img_info_input_blob:
-        feed_dict[img_info_input_blob] = [h, w, 1]
-
-    image = cv2.imread(args.input)
-
-    if args.labels:
-        with open(args.labels, 'r') as f:
-            labels_map = [x.strip() for x in f]
-    else:
         labels_map = None
 
-    print("To close the application, press 'CTRL+C' here or switch to the output window and press ESC key")
-    print("To switch between sync/async modes, press TAB key in the output window")
+        # Starting
+        frame_h, frame_w = image.shape[:2]
+        image_copy = image.copy()
+        image = cv2.resize(image, (self.w, self.h))
+        image = image.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+        image = image.reshape((self.n, self.c, self.h, self.w))
+        self.feed_dict[self.input_blob] = image
+        self.exec_net.start_async(request_id=0, inputs=self.feed_dict)
 
-    #Starting
-    start_time = time.time()
-    frame_h, frame_w = image.shape[:2]
-    image_copy = image.copy()
-    image = cv2.resize(image, (w, h))
-    image = image.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-    image = image.reshape((n, c, h, w))
-    feed_dict[input_blob] = image
-    exec_net.start_async(request_id=0, inputs=feed_dict)
+        # Parse detection results of the current request
+        self.exec_net.requests[0].wait(-1)
+        res = self.exec_net.requests[0].outputs[self.out_blob]
+        for obj in res[0][0]:
+            # Draw only objects when probability more than specified threshold
+            if obj[2] > 0.5: #THRESOLD!!!
+                xmin = int(obj[3] * frame_w)
+                ymin = int(obj[4] * frame_h)
+                xmax = int(obj[5] * frame_w)
+                ymax = int(obj[6] * frame_h)
+                class_id = int(obj[1])
+                # Draw box and label\class_id
+                color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
+                cv2.rectangle(image_copy, (xmin, ymin), (xmax, ymax), color, 2)
+                det_label = labels_map[class_id] if labels_map else str(class_id)
+                cv2.putText(image_copy, det_label + ' ' + str(round(obj[2] * 100, 1)) + ' %', (xmin, ymin - 7),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
 
-    # Parse detection results of the current request
-    exec_net.requests[0].wait(-1)
-
-    end_time = time.time()
-    print(end_time - start_time)
-    res = exec_net.requests[0].outputs[out_blob]
-    for obj in res[0][0]:
-        # Draw only objects when probability more than specified threshold
-        if obj[2] > args.prob_threshold:
-            xmin = int(obj[3] * frame_w)
-            ymin = int(obj[4] * frame_h)
-            xmax = int(obj[5] * frame_w)
-            ymax = int(obj[6] * frame_h)
-            class_id = int(obj[1])
-            # Draw box and label\class_id
-            color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
-            cv2.rectangle(image_copy, (xmin, ymin), (xmax, ymax), color, 2)
-            det_label = labels_map[class_id] if labels_map else str(class_id)
-            cv2.putText(image_copy, det_label + ' ' + str(round(obj[2] * 100, 1)) + ' %', (xmin, ymin - 7),
-                cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
-
-    cv2.imshow("Detection Results", image_copy)
-    cv2.waitKey()
-
-
-
-
-
-if __name__ == '__main__':
-    sys.exit(main() or 0)
+        cv2.imshow("Detection Results", image_copy)
+        cv2.waitKey()
