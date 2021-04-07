4608300a081e9652531631847730140d549c7aba
Mikhail Zoreev <mzoreev@gmail.com>
2021-04-07 10:40:54 +0300

    add coco_bus download & add metrics

---
+++ tests/metrics.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+
+class Metrics:
+    @staticmethod
+    def write(metric: str, value: Any):
+        file = open("metrics.txt", "a")
+        file.write(metric + " " + str(value))
