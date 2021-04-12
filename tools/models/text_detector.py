d3800dcf55c9045a6ef97ef4f2855acf7865f341
Andrey Dmitriev <andrey.dmitriev@intel.com>
2021-04-12 17:32:34 +0300

    Add interface text_detector

--- tools/models/text_detector.py
+++ tools/models/text_detector.py
@@ -1,7 +1,16 @@
 from abc import ABC
+import torch
+
+from tools.models.text_detectors.craft.text_craft_detector import CraftDetection
 
 
 class TextDetectorFactory(ABC):
     @staticmethod
     def get(name: str):
-        pass
+        if name == 'craft':
+            if torch.cuda.is_available():
+                return CraftDetection(True)
+            else:
+                return CraftDetection(False)
+        else:
+            raise RuntimeError('Wrong detector name ' + name)
---
+++ tools/models/text_detectors/craft/text_craft_detector.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from craft_text_detector import Craft, get_prediction
+import cv2
+
+from box import Box
+from tools.Interfaces.text_interface import ITextDetector
+
+
+class CraftDetection(ITextDetector):
+    def __init__(self, cuda: bool):
+        self.__model__ = Craft(crop_type="box", cuda=cuda, refiner=False, rectify=False)
+        self.__results__ = None
+
+    def prediction(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if image.shape[0] == 2:
+            image = image[0]
+        if image.shape[2] == 4:
+            image = image[:, :, :3]
+
+        self.__results__ = get_prediction(
+            image=image,
+            craft_net=self.__model__.craft_net,
+            text_threshold=self.__model__.text_threshold,
+            link_threshold=self.__model__.link_threshold,
+            low_text=self.__model__.low_text,
+            cuda=self.__model__.cuda,
+            long_size=self.__model__.long_size,
+        )
+
+    def get_boxes(self) -> List[Box]:
+        result_text_box = []
+        for box in self.__results__['boxes']:
+            result_text_box.append(Box((box[0][0], box[0][1]), box[2][0] - box[0][0], box[2][1] - box[0][1]))
+
+        return result_text_box
