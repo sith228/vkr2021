7f1d64586c6aaf6e4c1b85a4f12f77ebe50f0d81
Mikhail-Zoreev <mzoreev@gmail.com>
2021-04-13 16:25:46 +0300

    callbacks_list to private

--- common/event/publisher.py
+++ common/event/publisher.py
@@ -1,18 +1,18 @@
 class Publisher:
     def __init__(self):
-        self.callbacks = []
+        self.__callbacks = []
 
     def add_callback(self, callback):
         """
         Subscribe to publisher events, ask him to run callback
         :param callback: callback function
         """
-        self.callbacks.append(callback)
+        self.__callbacks.append(callback)
 
     def broadcast(self, message):
         """
         Runs listeners callbacks
         :param message:
         """
-        for callback in self.callbacks:
+        for callback in self.__callbacks:
             callback(message)  # TODO: Run callback at new thread
