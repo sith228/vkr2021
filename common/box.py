0558afec63b6458a15321d9a87d6c73a91c37d7f
Andrey Dmitriev <andrey.dmitriev@intel.com>
2021-04-14 23:58:39 +0300

    Added some parametrs for box container

--- common/box.py
+++ common/box.py
@@ -1,5 +1,5 @@
 import re
-from typing import List, Any
+from typing import List, Any, Tuple
 
 
 # TODO: Расширить класс бокс
@@ -15,14 +15,16 @@ class Box(object):
     :param W: width box
     :param H: height box
     :param list_of_inside_boxes: container other boxes
+
+    :method get_bbox: return Tuple(x, y, width, height) of box
     """
 
     def __init__(self, bound_box, w, h, angle=0):
         # Add more if can
-        self.bound_box_points = bound_box
+        self.bound_box_points: Tuple[int, int] = bound_box
         self.angle = angle
         self.__check_list__ = {}
-        self.text = {}
+        self.__text__ = ""
         self.height = h  # height of box
         self.width = w  # width of box
         self.__list_of_inside_boxes__ = []  # contain other boxes
@@ -39,6 +41,15 @@ class Box(object):
         else:
             return "number"
 
+    def get_bbox(self) -> Tuple[int, int, int, int]:
+        return self.bound_box_points[0], self.bound_box_points[1], self.width, self.height
+
+    def set_text(self, text: str):
+        self.__text__ = text
+
+    def get_text(self) -> str:
+        return self.__text__
+
     def insert_box(self, list_boxes):
         """
         Resizes image with keeping aspect ratio
@@ -49,16 +60,3 @@ class Box(object):
 
     def get_subboxes(self) -> List[Any]:
         return self.__list_of_inside_boxes__
-
-    # TODO: Необходимо удалить или изменить
-    def get_vino_check(self) -> bool:
-        return self.__check_list__["VINO"]
-
-    def get_tesseract_check(self) -> bool:
-        return self.__check_list__["TESS"]
-
-    def get_bin_text(self) -> bool:
-        return self.__check_list__["BIN"]
-
-    def get_match_check(self) -> bool:
-        return self.__check_list__["HAVE_MATCH"]
