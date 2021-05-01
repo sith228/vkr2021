f8010250d23f02735b66f272388b7c7259769417
Mikhail-Zoreev <mzoreev@gmail.com>
2021-05-01 10:33:19 +0300

    Change regex

--- common/utils/box_validator_utils.py
+++ common/utils/box_validator_utils.py
@@ -21,5 +21,5 @@ class BoxValidator:
 
     @staticmethod
     def has_valid_text(text_box: TextBox):  # TODO: Only for Nizniy Novgorod
-        return re.match('\\S\\d+', text_box.get_text())
+        return re.match('\\d*', text_box.get_text())
 
