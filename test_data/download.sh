039f1457362e169364dea7458845fb73c180fe13
Mikhail-Zoreev <mzoreev@gmail.com>
2021-05-07 11:28:14 +0300

    Ci ip update

--- test_data/download.sh
+++ test_data/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 # Download
-wget "35.239.156.163:8080/download/test_data.zip" # Own test images
-wget "35.239.156.163:8080/download/coco_bus.zip"  # Bus images from COCO dataset
+wget "34.67.40.164:8080/download/test_data.zip" # Own test images
+wget "34.67.40.164:8080/download/coco_bus.zip"  # Bus images from COCO dataset
 
 # Extract
 unzip -q test_data.zip -d "$BASEDIR"
