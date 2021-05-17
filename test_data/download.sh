17edb8a2fc61f2b522f49be97fef6d70a75b0092
Andrey Dmitriev <deseq9767@gmail.com>
2021-05-17 17:33:58 +0300

    Added tests for bus rout number pipeline

--- test_data/download.sh
+++ test_data/download.sh
@@ -4,8 +4,10 @@ BASEDIR=$(dirname "$0")
 wget "34.67.40.164:8080/download/test_data.zip" # Own test images
 wget "34.67.40.164:8080/download/coco_bus.zip"  # Bus images from COCO dataset
 wget "34.67.40.164:8080/download/test_number.zip" # Cropped image
+wget "34.67.40.164:8080/download/pipeline_test_data.zip" # Image for test pipeline
 
 # Extract
 unzip -q test_data.zip -d "$BASEDIR"
 unzip -q test_number.zip -d "$BASEDIR"
+unzip -q pipeline_test_data.zip "$BASEDIR"
 unzip -q coco_bus.zip -d "$BASEDIR/coco_bus"
